6d0f1116212d481f8c074a16ed9653d4077a8475
ape03 <kai.henssen@googlemail.com>
2019-03-20 16:16:19 +0000

    evict cache

--- src/main/java/com/develogical/CachedWeatherService.java
+++ src/main/java/com/develogical/CachedWeatherService.java
@@ -4,29 +4,73 @@ import com.weather.Day;
 import com.weather.Forecast;
 import com.weather.Region;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CachedWeatherService implements WeatherService {
-
-    private WeatherServiceAdaptor adapter;
-
+    private WeatherService adapter;
     private Map<Region, Map<Day, Forecast>> cache = new HashMap<>();
+    private List<SearchKey> ordering = new ArrayList<>();
+    private int cacheSize = 10;
 
-    public CachedWeatherService(WeatherServiceAdaptor adapter) {
+    public CachedWeatherService(WeatherService adapter) {
         this.adapter = adapter;
-
-
     }
 
     @Override
     public Forecast getWeather(Region region, Day day) {
-        if(cache.get(region) != null && cache.get(region).get(day) != null) {
+
+        SearchKey sk = new SearchKey(region, day);
+        if (ordering.contains(sk)){
+            ordering.remove(sk);
+        }
+        ordering.add(sk);
+
+        while (ordering.size() > cacheSize) {
+            SearchKey oldest = ordering.remove(0);
+            cache.get(oldest.region).remove(oldest.day);
+
+        }
+
+        if(cache.containsKey(region) && cache.get(region).containsKey(day)) {
             return cache.get(region).get(day);
         }
         Forecast forecast = adapter.getWeather(region, day);
-        cache.put(region, new HashMap<Day, Forecast>());
+        if (!cache.containsKey(region)) {
+            cache.put(region, new HashMap<Day, Forecast>());
+        }
         cache.get(region).put(day,forecast);
         return forecast;
+    }
+
+    public void setCacheSize(int i) {
+        this.cacheSize = i;
+    }
+
+
+    private static class SearchKey {
+        Region region;
+        Day day;
+
+        public SearchKey(Region region, Day day) {
+            this.region = region;
+            this.day = day;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof SearchKey)){
+                return  false;
+            }
+            SearchKey sk = (SearchKey) o;
+            return  sk.region == this.region && sk.day == this.day;
+        }
+
+        @Override
+        public int hashCode() {
+            return region.hashCode() * day.hashCode();
+        }
     }
 }
--- src/test/java/com/develogical/CachedWeatherServiceTest.java
+++ src/test/java/com/develogical/CachedWeatherServiceTest.java
@@ -5,59 +5,63 @@ import com.weather.Forecast;
 import com.weather.Region;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class CachedWeatherServiceTest {
-
-
     @Test
     public void testCacheAfterFill(){
-
-        WeatherServiceAdaptor adapter = mock(WeatherServiceAdaptor.class);
-        when(adapter.getWeather(Region.LONDON, Day.SATURDAY)).thenReturn(mock(Forecast.class));
+        WeatherService adapter = mock(WeatherService.class);
+        Forecast hot = new Forecast("hot", 44);
+        when(adapter.getWeather(Region.LONDON, Day.SATURDAY)).thenReturn(hot);
 
         CachedWeatherService cachedWeatherService = new CachedWeatherService(adapter);
 
-        cachedWeatherService.getWeather(Region.LONDON, Day.SATURDAY);
-
+        assertThat(cachedWeatherService.getWeather(Region.LONDON, Day.SATURDAY), is(hot));
         verify(adapter).getWeather(Region.LONDON, Day.SATURDAY);
 
-        cachedWeatherService.getWeather(Region.LONDON, Day.SATURDAY);
+        assertThat(cachedWeatherService.getWeather(Region.LONDON, Day.SATURDAY), is(hot));
         verifyNoMoreInteractions(adapter);
-
     }
 
     @Test
     public void testCacheTwoEntries(){
-
-        WeatherServiceAdaptor adapter = mock(WeatherServiceAdaptor.class);
+        WeatherService adapter = mock(WeatherService.class);
         when(adapter.getWeather(any(Region.class), any(Day.class))).thenReturn(mock(Forecast.class));
-
 
         CachedWeatherService cachedWeatherService = new CachedWeatherService(adapter);
 
-
         cachedWeatherService.getWeather(Region.LONDON, Day.SATURDAY);
-
         verify(adapter).getWeather(Region.LONDON, Day.SATURDAY);
 
         cachedWeatherService.getWeather(Region.LONDON, Day.SATURDAY);
 
         verifyNoMoreInteractions(adapter);
-
 
         cachedWeatherService.getWeather(Region.EDINBURGH, Day.SUNDAY);
 
         verify(adapter).getWeather(Region.EDINBURGH, Day.SUNDAY);
 
-
         cachedWeatherService.getWeather(Region.EDINBURGH, Day.SUNDAY);
 
         verifyNoMoreInteractions(adapter);
-
-
     }
 
+    @Test
+    public void testCacheLimit(){
+        WeatherService adapter = mock(WeatherService.class);
+        when(adapter.getWeather(any(Region.class), any(Day.class))).thenReturn(mock(Forecast.class));
 
+        CachedWeatherService cachedWeatherService = new CachedWeatherService(adapter);
+
+        cachedWeatherService.setCacheSize(1);
+
+        cachedWeatherService.getWeather(Region.LONDON, Day.SATURDAY);
+
+        cachedWeatherService.getWeather(Region.LONDON, Day.SUNDAY);
+
+        cachedWeatherService.getWeather(Region.LONDON, Day.SATURDAY);
+        verify(adapter, times(2)).getWeather(Region.LONDON, Day.SATURDAY);
+    }
 }
